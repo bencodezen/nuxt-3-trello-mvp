11783775ed787a845220f69ce1b22eef05425fdb
Ben Hong <ben@bencodezen.io>
2021-11-02 13:58:44 -0500

    feature: add ability to create columns on board in workspace

--- pages/workspace/[id].vue
+++ pages/workspace/[id].vue
@@ -8,8 +8,19 @@ export default {
     }
   },
   data: () => ({
-    workspaceName: ''
+    workspaceName: '',
+    board: {
+      name: 'Cherries',
+      columns: []
+    }
   }),
+  methods: {
+    createColumn() {
+      this.board.columns.push({
+        items: []
+      })
+    }
+  },
   mounted() {
     this.workspaceName = this.workspaceList.find(
       workspace => workspace.id === Number(this.$route.params.id)
@@ -21,7 +32,26 @@ export default {
 <template>
   <div>
     <h1>{{ workspaceName }} Workspace (#{{ $route.params.id }})</h1>
+    <section>
+      <h2>{{ board.name }}</h2>
+      <button @click="createColumn">Create Column</button>
+      <div class="column-grid">
+        <ul class="board-column" v-for="column in board.columns"></ul>
+      </div>
+    </section>
   </div>
 </template>
 
-<style></style>
+<style>
+.column-grid {
+  display: grid;
+  grid-template-columns: repeat(v-bind(board.columns.length), 1fr);
+}
+
+.board-column {
+  border: 1px solid #222;
+  height: 80vh;
+  width: 100px;
+  margin-right: 1rem;
+}
+</style>

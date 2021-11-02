88b89f6a533c491babbd135f30c5519a59089c86
Ben Hong <ben@bencodezen.io>
2021-11-02 13:44:37 -0500

    feature: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <div>
+    <p>Default Layout</p>
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link>
+    </nav>
+    <slot />
+  </div>
+</template>
+
+<style></style>

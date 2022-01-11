089af9bc3ea9d8956226151c0289f5b48bfcb6dd
Ben Hong <ben@bencodezen.io>
2022-01-11 12:28:21 -0500

    feature: add styles for columns

--- pages/workspace/[id].vue
+++ pages/workspace/[id].vue
@@ -40,38 +40,61 @@ export default {
 </script>
 
 <template>
-  <div>
+  <main class="workspace-page">
     <h1>{{ workspaceName }} Workspace (#{{ $route.params.id }})</h1>
     <section>
-      <button @click="createColumn">Create Column</button>
+      <button @click="createColumn" style="margin-bottom: 15px">
+        Create Column
+      </button>
       <div class="column-grid">
         <section class="board-column" v-for="column in board.columns">
           <input
             v-model="column.newItemName"
             @keyup.enter="createCard(column)"
             type="text"
+            style="padding: 5px; font-size: 0.9rem; margin-bottom: 10px"
+            placeholder="New card title"
           />
-          <button @click="createCard(column)">Create Card</button>
-          <ul>
-            <li v-for="item in column.items" :key="item.id">
+          <button @click="createCard(column)" style="margin-bottom: 15px">
+            Create Card
+          </button>
+          <ul style="margin: 0; padding: 0">
+            <li v-for="item in column.items" :key="item.id" class="base-card">
               {{ item.name }}
             </li>
           </ul>
         </section>
       </div>
     </section>
-  </div>
+  </main>
 </template>
 
 <style>
+.base-card {
+  border: 1px solid #222;
+  padding: 10px;
+  list-style: none;
+  background-color: #fff;
+}
+
 .column-grid {
   display: grid;
   grid-template-columns: repeat(v-bind(board.columns.length), 1fr);
 }
 
 .board-column {
+  background-color: #eee;
   border: 1px solid #222;
   height: 80vh;
   margin-right: 1rem;
+  padding: 10px;
+}
+
+.workspace-page {
+  padding: 30px;
+}
+
+.workspace-page h1 {
+  margin-top: 0;
 }
 </style>

eb205ca95c1064ff912377123f6c1b31fed72106
Ben Hong <ben@bencodezen.io>
2022-01-11 12:19:28 -0500

    feature: add styles to page header

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,42 @@ export default {}
 
 <template>
   <div>
-    <nav>
-      <nuxt-link to="/">Home</nuxt-link>
-    </nav>
+    <header class="main-header">
+      <p class="logo">Trello MVP Clone with Nuxt 3</p>
+      <nav class="main-nav">
+        <nuxt-link to="/">Home</nuxt-link>
+      </nav>
+    </header>
+
     <slot />
   </div>
 </template>
 
-<style></style>
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Roboto', sans-serif;
+}
+
+.main-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #41b883;
+  padding: 15px;
+}
+
+.main-nav a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.logo {
+  color: #fff;
+  font-weight: bold;
+  margin: 0;
+  padding: 0;
+}
+</style>
